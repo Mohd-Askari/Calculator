1d8cbdfc880c80b16e7bcee1a75d65f78a517e74
Mohd-Askari <114907734+Mohd-Askari@users.noreply.github.com>
2023-03-14 00:50:49 +0530

    Add files via upload
    This is Calculators..

---
+++ calculator/index.js
@@ -0,0 +1,23 @@
+let screen=" ";
+let buttons = document.querySelectorAll('.button')
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            screen=eval(screen);
+            document.querySelector('input').value=screen;
+
+        }
+        else if(e.target.innerHTML=='AC'){
+            screen=" ";
+            document.querySelector('input').value=screen;
+
+        }
+        else{
+            console.log(e.target);
+            screen =screen +e.target.innerHTML;
+            document.querySelector('input').value=screen;
+    
+        }
+       
+    })
+})
